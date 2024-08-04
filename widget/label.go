f7de9ff2d6e94115cb005d82045096bc6f23ec45
Drew Weymouth <dweymouth@gmail.com>
2024-08-04 08:13:28 -0700

    add API for Label and RichText to propagate received hover events

--- widget/label.go
+++ widget/label.go
@@ -3,13 +3,26 @@ package widget
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 )
 
 // Label widget is a label component with appropriate padding and layout.
+//
+// NOTE: since the tool tip label implements desktop.Hoverable while the
+// standard Label does not, this widget may result in hover events not
+// reaching the parent Hoverable widget. It provides a callback API to allow
+// parent widgets to be notified of hover events received on this widget.
 type Label struct {
 	widget.Label
 	ToolTipWidgetExtend
+
+	// Sets a callback that will be invoked for MouseIn events received
+	OnMouseIn func(*desktop.MouseEvent)
+	// Sets a callback that will be invoked for MouseMoved events received
+	OnMouseMoved func(*desktop.MouseEvent)
+	// Sets a callback that will be invoked for MouseOut events received
+	OnMouseOut func()
 }
 
 // NewLabel creates a new label widget with the set text content
@@ -42,4 +55,25 @@ func NewLabelWithStyle(text string, alignment fyne.TextAlign, style fyne.TextSty
 func (l *Label) ExtendBaseWidget(wid fyne.Widget) {
 	l.ExtendToolTipWidget(wid)
 	l.Label.ExtendBaseWidget(wid)
+}
+
+func (l *Label) MouseIn(e *desktop.MouseEvent) {
+	l.ToolTipWidgetExtend.MouseIn(e)
+	if f := l.OnMouseIn; f != nil {
+		f(e)
+	}
+}
+
+func (l *Label) MouseMoved(e *desktop.MouseEvent) {
+	l.ToolTipWidgetExtend.MouseMoved(e)
+	if f := l.OnMouseMoved; f != nil {
+		f(e)
+	}
+}
+
+func (l *Label) MouseOut() {
+	l.ToolTipWidgetExtend.MouseOut()
+	if f := l.OnMouseOut; f != nil {
+		f()
+	}
 }
--- widget/richtext.go
+++ widget/richtext.go
@@ -3,13 +3,26 @@ package widget
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 )
 
 // RichText represents the base element for a rich text-based widget.
+//
+// NOTE: since the tool tip RichText implements desktop.Hoverable while the
+// standard RichText does not, this widget may result in hover events not
+// reaching the parent Hoverable widget. It provides a callback API to allow
+// parent widgets to be notified of hover events received on this widget.
 type RichText struct {
 	widget.RichText
 	ToolTipWidgetExtend
+
+	// Sets a callback that will be invoked for MouseIn events received
+	OnMouseIn func(*desktop.MouseEvent)
+	// Sets a callback that will be invoked for MouseMoved events received
+	OnMouseMoved func(*desktop.MouseEvent)
+	// Sets a callback that will be invoked for MouseOut events received
+	OnMouseOut func()
 }
 
 // NewRichText returns a new RichText widget that renders the given text and segments.
@@ -33,4 +46,25 @@ func NewRichTextWithText(text string) *RichText {
 func (r *RichText) ExtendBaseWidget(wid fyne.Widget) {
 	r.ExtendToolTipWidget(wid)
 	r.RichText.ExtendBaseWidget(wid)
+}
+
+func (r *RichText) MouseIn(e *desktop.MouseEvent) {
+	r.ToolTipWidgetExtend.MouseIn(e)
+	if f := r.OnMouseIn; f != nil {
+		f(e)
+	}
+}
+
+func (r *RichText) MouseMoved(e *desktop.MouseEvent) {
+	r.ToolTipWidgetExtend.MouseMoved(e)
+	if f := r.OnMouseMoved; f != nil {
+		f(e)
+	}
+}
+
+func (r *RichText) MouseOut() {
+	r.ToolTipWidgetExtend.MouseOut()
+	if f := r.OnMouseOut; f != nil {
+		f()
+	}
 }
